3e4de3da4b0dc532540fb71e078fb6047ef61858
Jawher Moussa <jawher.moussa@gmail.com>
2010-08-13 01:25:00 +0100

    Added a bunch of moulders factory methods as they're terset and nicer-looking than class instantiation

---
+++ src/main/java/jawher/moulder/moulds/Moulds.java
@@ -0,0 +1,76 @@
+package jawher.moulder.moulds;
+
+import jawher.moulder.Value;
+import jawher.moulder.values.SimpleValue;
+
+import org.jsoup.nodes.Node;
+
+public class Moulds {
+	public static Appender append(Value<Iterable<Node>> html) {
+		return new Appender(html);
+	}
+
+	public static Appender append(String html) {
+		return new Appender(html);
+	}
+
+	public static Prepender prepend(Value<Iterable<Node>> html) {
+		return new Prepender(html);
+	}
+
+	public static Prepender prepend(String html) {
+		return new Prepender(html);
+	}
+
+	public static ChildAppender appendToChildren(Value<Iterable<Node>> html) {
+		return new ChildAppender(html);
+	}
+
+	public static ChildAppender appendToChildren(String html) {
+		return new ChildAppender(html);
+	}
+
+	public static ChildAppender prependToChildren(Value<Iterable<Node>> html) {
+		return new ChildAppender(html);
+	}
+
+	public static ChildPrepender prependToChildren(String html) {
+		return new ChildPrepender(html);
+	}
+
+	public static Texter text(String text) {
+		return new Texter(text);
+	}
+
+	public static Texter text(Value<String> text) {
+		return new Texter(text);
+	}
+
+	public static <T> Repeater<T> repeat(Iterable<T> items) {
+		return new Repeater<T>(new SimpleValue<Iterable<T>>(items));
+	}
+	
+	public static <T> Repeater<T> repeat(Value<Iterable<T>> items) {
+		return new Repeater<T>(items);
+	}
+
+	public static Remover remove() {
+		return new Remover();
+	}
+
+	public static Remover remove(Value<Boolean> remove) {
+		return new Remover(remove);
+	}
+
+	public static AttrModifier attr(String attr, String value) {
+		return new AttrModifier(attr, value);
+	}
+
+	public static AttrModifier attr(String attr, Value<String> value) {
+		return new AttrModifier(attr, value);
+	}
+
+	public static AttrModifier attr(Value<String> attr, Value<String> value) {
+		return new AttrModifier(attr, value);
+	}
+}
