ec90a81367a4c039755d311598bebe22bf7af9e5
Jawher Moussa <jawher.moussa@gmail.com>
2010-08-14 02:21:31 +0100

    Rewrote MoulderShop so that it delegates to a SubMoulder

--- src/main/java/jawher/moulder/MoulderShop.java
+++ src/main/java/jawher/moulder/MoulderShop.java
@@ -1,70 +1,29 @@
 package jawher.moulder;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import jawher.moulder.moulds.SubMoulder;
+
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.select.Elements;
 
 public class MoulderShop {
-	private static final class TemplatorConfig {
-		private String selector;
-		private List<Moulder> templators;
+	private SubMoulder subMoulder = new SubMoulder();
 
-		public TemplatorConfig(String selector, List<Moulder> templators) {
-			super();
-			this.selector = selector;
-			this.templators = templators;
-		}
+	public MoulderShop register(String selector, Moulder... moulders) {
+		subMoulder.register(selector, Arrays.asList(moulders));
+		return this;
 	}
 
-	private List<TemplatorConfig> cfg = new ArrayList<TemplatorConfig>();
-
-	public void register(String selector, Moulder... templators) {
-		register(selector, Arrays.asList(templators));
-	}
-
-	public void register(String selector, List<Moulder> templators) {
-		cfg.add(new TemplatorConfig(selector, templators));
+	public MoulderShop register(String selector, List<Moulder> templators) {
+		subMoulder.register(selector, templators);
+		return this;
 	}
 
 	public void process(Document doc) {
 		MoulderUtils factory = new MoulderUtils(doc);
-		
-		for (TemplatorConfig c : cfg) {
-			Elements elements = doc.select(c.selector);
-			for (Element e : elements) {
-				List<NodeAndData> oes = Arrays
-						.asList(new NodeAndData(e));
-				for (Moulder templator : c.templators) {
-					List<NodeAndData> tes = new ArrayList<NodeAndData>();
-					for (NodeAndData oe : oes) {
-						if (oe.node instanceof Element) {
-							List<NodeAndData> processed = templator
-									.process(new ElementAndData(
-											(Element) oe.node, oe.data), factory);
-							for (NodeAndData processedNode : processed) {
-								tes.add(new NodeAndData(
-										processedNode.node, processedNode.data));
-							}
-						} else {
-							tes.add(oe);
-						}
 
-					}
-					oes = tes;
-				}
-				// replace e with oes
-				for (NodeAndData oe : oes) {
-					e.before(oe.node.outerHtml());
-				}
-
-				e.remove();
-			}
-		}
+		subMoulder.process(new ElementAndData(doc), factory);
 	}
 
 }
