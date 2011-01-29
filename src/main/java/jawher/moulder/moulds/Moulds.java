ecd6b149654249f9dd253c00e195a5956b6a5b6b
Jawher Moussa <jawher.moussa@gmail.com>
2011-01-29 21:45:33 +0100

    Added factory methods for SubMoulder

--- src/main/java/jawher/moulder/moulds/Moulds.java
+++ src/main/java/jawher/moulder/moulds/Moulds.java
@@ -6,98 +6,107 @@ import jawher.moulder.values.SimpleValue;
 
 import org.jsoup.nodes.Node;
 
+import java.util.List;
+
 /**
  * A class with a bunch of static factory methods to common moulders
- * 
+ *
  * @author jawher
- * 
  */
 public class Moulds {
-	public static Appender append(Value<Iterable<Node>> html) {
-		return new Appender(html);
-	}
+    public static Appender append(Value<Iterable<Node>> html) {
+        return new Appender(html);
+    }
 
-	public static Appender append(String html) {
-		return new Appender(html);
-	}
+    public static Appender append(String html) {
+        return new Appender(html);
+    }
 
-	public static Prepender prepend(Value<Iterable<Node>> html) {
-		return new Prepender(html);
-	}
+    public static Prepender prepend(Value<Iterable<Node>> html) {
+        return new Prepender(html);
+    }
 
-	public static Prepender prepend(String html) {
-		return new Prepender(html);
-	}
+    public static Prepender prepend(String html) {
+        return new Prepender(html);
+    }
 
-	public static Replacer replace(Value<Iterable<Node>> html) {
-		return new Replacer(html);
-	}
+    public static Replacer replace(Value<Iterable<Node>> html) {
+        return new Replacer(html);
+    }
 
-	public static Replacer replace(String html) {
-		return new Replacer(html);
-	}
+    public static Replacer replace(String html) {
+        return new Replacer(html);
+    }
 
-	public static ChildAppender appendToChildren(Value<Iterable<Node>> html) {
-		return new ChildAppender(html);
-	}
+    public static ChildAppender appendToChildren(Value<Iterable<Node>> html) {
+        return new ChildAppender(html);
+    }
 
-	public static ChildAppender appendToChildren(String html) {
-		return new ChildAppender(html);
-	}
+    public static ChildAppender appendToChildren(String html) {
+        return new ChildAppender(html);
+    }
 
-	public static ChildAppender prependToChildren(Value<Iterable<Node>> html) {
-		return new ChildAppender(html);
-	}
+    public static ChildAppender prependToChildren(Value<Iterable<Node>> html) {
+        return new ChildAppender(html);
+    }
 
-	public static ChildPrepender prependToChildren(String html) {
-		return new ChildPrepender(html);
-	}
+    public static ChildPrepender prependToChildren(String html) {
+        return new ChildPrepender(html);
+    }
 
-	public static Texter text(String text) {
-		return new Texter(text);
-	}
+    public static Texter text(String text) {
+        return new Texter(text);
+    }
 
-	public static Texter text(Value<String> text) {
-		return new Texter(text);
-	}
+    public static Texter text(Value<String> text) {
+        return new Texter(text);
+    }
 
-	public static <T> Repeater<T> repeat(Iterable<T> items) {
-		return new Repeater<T>(new SimpleValue<Iterable<T>>(items));
-	}
+    public static <T> Repeater<T> repeat(Iterable<T> items) {
+        return new Repeater<T>(new SimpleValue<Iterable<T>>(items));
+    }
 
-	public static <T> Repeater<T> repeat(Value<Iterable<T>> items) {
-		return new Repeater<T>(items);
-	}
+    public static <T> Repeater<T> repeat(Value<Iterable<T>> items) {
+        return new Repeater<T>(items);
+    }
 
-	public static Remover remove() {
-		return new Remover();
-	}
+    public static Remover remove() {
+        return new Remover();
+    }
 
-	public static Remover remove(Value<Boolean> remove) {
-		return new Remover(remove);
-	}
+    public static Remover remove(Value<Boolean> remove) {
+        return new Remover(remove);
+    }
 
-	public static AttrModifier attr(String attr, String value) {
-		return new AttrModifier(attr, value);
-	}
+    public static AttrModifier attr(String attr, String value) {
+        return new AttrModifier(attr, value);
+    }
 
-	public static AttrModifier attr(String attr, Value<String> value) {
-		return new AttrModifier(attr, value);
-	}
+    public static AttrModifier attr(String attr, Value<String> value) {
+        return new AttrModifier(attr, value);
+    }
 
-	public static AttrModifier attr(Value<String> attr, Value<String> value) {
-		return new AttrModifier(attr, value);
-	}
+    public static AttrModifier attr(Value<String> attr, Value<String> value) {
+        return new AttrModifier(attr, value);
+    }
 
-	public static NopMoulder nop() {
-		return new NopMoulder();
-	}
-	
-	public static IfMoulder ifm(Value<Boolean> condition, Moulder thenMoulder, Moulder elseMoulder) {
-		return new IfMoulder(condition, thenMoulder, elseMoulder);
-	}
-	
-	public static IfMoulder ifm(boolean condition, Moulder thenMoulder, Moulder elseMoulder) {
-		return new IfMoulder(condition, thenMoulder, elseMoulder);
-	}
+    public static NopMoulder nop() {
+        return new NopMoulder();
+    }
+
+    public static IfMoulder ifm(Value<Boolean> condition, Moulder thenMoulder, Moulder elseMoulder) {
+        return new IfMoulder(condition, thenMoulder, elseMoulder);
+    }
+
+    public static IfMoulder ifm(boolean condition, Moulder thenMoulder, Moulder elseMoulder) {
+        return new IfMoulder(condition, thenMoulder, elseMoulder);
+    }
+
+    public static SubMoulder sub(String selector, Moulder... moulders) {
+        return new SubMoulder().register(selector, moulders);
+    }
+
+    public static SubMoulder sub(String selector, List<Moulder> moulders) {
+        return new SubMoulder().register(selector, moulders);
+    }
 }
